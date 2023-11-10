5b684f0b42865e315506bda9bfd173b84b953cea
Nayoon <nayoonk928@gmail.com>
2023-11-10 15:30:21 +0900

    feat #7 - 예외처리 코드 구현

---
+++ src/main/java/com/example/budget/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.budget.exception;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String message;
+}
