f5fb3c36ec0971161db0dfa8b33ba54140676c6f
Nayoon <nayoonk928@gmail.com>
2023-11-11 18:39:41 +0900

    test #2 - 카테고리 목록 조회 테스트 구현 - 테스트 DB에 카테고리 데이터 저장하는 코드 구현 - 카테고리 목록 조회 API 테스트 코드 구현 - 카테고리 목록 조회 단위 테스트 코드 구현

---
+++ src/test/java/com/example/budget/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.example.budget.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.budget.dto.res.CategoriesResDto;
+import com.example.budget.entity.Category;
+import com.example.budget.repository.CategoryRepository;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+
+  @InjectMocks
+  private CategoryServiceImpl categoryService;
+
+  @Mock
+  private CategoryRepository categoryRepository;
+
+  @Test
+  @DisplayName("카테고리 목록 불러오기")
+  void get_categories() {
+    //given
+    Category category1 = new Category(1L, "카테고리1", 0.0);
+    Category category2 = new Category(2L, "카테고리2", 0.0);
+    List<Category> mockCategories = Arrays.asList(category1, category2);
+
+    Mockito.when(categoryRepository.findAll()).thenReturn(mockCategories);
+
+    //when
+    CategoriesResDto result = categoryService.getCategories();
+
+    //then
+    assertEquals(2, result.categories().size());
+    assertEquals(category1.getId(), result.categories().get(0).id());
+    assertEquals(category2.getId(), result.categories().get(1).id());
+  }
+
+}
